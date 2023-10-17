447750cadebf5b9e1e00b5252f96709567184d69
luisgabriel1 <oluisgabriel02@gmail.com>
2023-10-17 16:32:45 -0300

    .

--- src/resource/script/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('menuButton').addEventListener('click', function() {
-    var nav = document.getElementById('nav');
-    if (nav.classList.contains('active')) {
-        nav.classList.remove('active');
-    } else {
-        nav.classList.add('active');
-    }
-});
